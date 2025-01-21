0058f1e244291aaf83a09bf63c3825f5454a534a
Viktor Baranov <baranov.viktor.27@gmail.com>
2025-01-21 10:13:16 +0300

    Update parsing logic

--- pkg/antispam/body.go
+++ pkg/antispam/body.go
@@ -38,7 +38,8 @@ func checkText(str string, location string) []Detection {
 		strings.Contains(str_lower_case, "i have transferred") ||
 		strings.Contains(str_lower_case, "i don't receive") ||
 		strings.Contains(str_lower_case, "i didn't receive") ||
-		strings.Contains(str_lower_case, "binance") {
+		strings.Contains(str_lower_case, "binance") ||
+		strings.Contains(str_lower_case, "coinbase wallet") {
 		detections = append(detections, Detection{
 			Location:       location,
 			DebugInfo:      "Body contains info of failed transfer",
