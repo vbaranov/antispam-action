a09b85db259d4144bbbe4c8311d2b56f16248979
Victor Baranov <baranov.viktor.27@gmail.com>
2025-10-02 22:12:34 +0300

    Update parsing logic

--- pkg/antispam/body.go
+++ pkg/antispam/body.go
@@ -80,7 +80,7 @@ func checkText(str string, location string, comment_author string) []Detection {
 			// Funds not received / transaction issues (expanded)
 			"transaction failed", "transaction not successful", "unsuccessful transaction", "failed transfer", "transfer failed", "transfer was not successful",
 			"transfer taking very long time", "transfer taking too long", "transfer taking a long time",
-			"transaction didn't go", "i was debited",
+			"transaction didn't go", "i was debited", "crypto didn’t transfer",
 			"reverted", "execution reverted", "tx reverted", "contract execution failed", "status failed",
 			"not credited", "never credited", "credit not appearing", "deposit confirmed but not credited", "confirmed on chain not credited",
 			"did not recieve payment", "did not receive payment", "payment not received", "not get payment", "not receiving money",
