a70cc7c1cae099c164180577531aa793b333d46c
Victor Baranov <baranov.viktor.27@gmail.com>
2026-09-24 12:02:39 +0300

    Update parsing logic

--- pkg/antispam/body.go
+++ pkg/antispam/body.go
@@ -113,7 +113,7 @@ func checkText(str string, location string, comment_author string) []Detection {
 			"recover the fund", "fund is not in my", "cancel the transaction", "i withrawed", "withdrawal failed", "cancel transaction",
 			"successful transaction but not delivered", "pending transaction", "send to etherum", "send plume", "got lost", "wrong supported wallet",
 			"to pay me", "gold coin", "purchase", "coins are in transit", "coin is in transit", "hasn't appear in wallet", "unknown withdraw", "trading",
-			"stole my tokens", "money never came", "accidentally deposited", "hack my wallet", "item havent received", "wallet is not open", "wallet is compromised",
+			"stole my tokens", "money never came", "accidentally deposited", "hack my wallet", "item havent received", "wallet is not open", "wallet is compromised", "never arrived",
 		}
 
 		for _, pattern := range patterns {
